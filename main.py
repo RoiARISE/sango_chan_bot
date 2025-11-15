93b52a68acd7cb37c51cdf13dee1ce6aa898bc88
敷島ロイ <160084982+RoiARISE@users.noreply.github.com>
2025-11-15 22:54:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ ADMIN_ID = config.ADMIN_ID
 if __name__ == "__main__":
     sango_chan = MyBot()
     try:
-        asyncio.run(sango_chan.run())
+        asyncio.run(sango_chan.main_task())
     except KeyboardInterrupt:
         msk.notes_create(text="うとうと……")
         print("botを停止します")
