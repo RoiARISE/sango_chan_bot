fc43d575281c778e3c702910ce2f4d6d030f4277
敷島ロイ <kyouyou000@gmail.com>
2025-11-16 13:28:44 +0900

    なんか足りないとこがあったので変更

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 env_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path=env_path)
+
 # --- Misskey API関連 ---
 TOKEN = os.getenv('TOKEN')
 INSTANCE_URL = os.getenv('INSTANCE')
